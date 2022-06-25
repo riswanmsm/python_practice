2f5347e1aeb232c157f0a045ffe19cdf3f76f32e
riswanmsm <msmr1986@gmail.com>
2022-06-25 08:37:28 +0530

    secret number guess

--- netacad/guess_secret_number.py
+++ netacad/guess_secret_number.py
@@ -1,17 +1,17 @@
-secret_number = 777
-
-print(
-"""
-+================================+
-| Welcome to my game, muggle!    |
-| Enter an integer number        |
-| and guess what number I've     |
-| picked for you.                |
-| So, what is the secret number? |
-+================================+
-""")
-
 def guess_secret_number():
+    
+    secret_number = 777
+
+    print(
+    """
+    +================================+
+    | Welcome to my game, muggle!    |
+    | Enter an integer number        |
+    | and guess what number I've     |
+    | picked for you.                |
+    | So, what is the secret number? |
+    +================================+
+    """)
     
     # take input from user
     guessed_number = int(input("Enter your guessed number: "))
@@ -27,5 +27,5 @@ def guess_secret_number():
     # If the number guessed matches the secret number display secret number and a message
     print("The secret number was:", secret_number)
     print("Well done, muggle! You are free now.")
-    
+
 guess_secret_number()
