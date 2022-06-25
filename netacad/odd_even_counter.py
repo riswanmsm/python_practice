61301846c2adf0004cb78f761c54815fac3ca6f9
riswanmsm <msmr1986@gmail.com>
2022-06-25 07:56:59 +0530

    odd even counter

---
+++ netacad/odd_even_counter.py
@@ -0,0 +1,22 @@
+def odd_even_counter():
+    # take a number as user input
+    number = int(input('Enter a number to continue or 0 to exit: '))
+    # initialize odd and even counters with 0
+    odd_counter = even_counter = 0
+    # loop and take number until user enters 0
+    while number:
+        
+        if number % 2 != 0:
+            # if the number is odd increase odd counter by one
+            odd_counter += 1
+        else:
+            # if the number is even increase even counter by one
+            even_counter += 1
+        
+        # take a number as user input
+        number = int(input('Enter a number to continue or 0 to exit: '))
+    
+    # print the odd counter and even counter
+    print("Number of odd numbers entered: ", odd_counter, "\n", "Number of even numbers entered: ", even_counter, sep="") 
+
+odd_even_counter()
